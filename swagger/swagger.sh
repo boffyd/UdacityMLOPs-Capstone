04e6f59ace6df09a2ddf840b541833b9c2cf3941
boffyd <72591620+boffyd@users.noreply.github.com>
2021-06-05 14:19:36 +1000

    Update swagger.sh

--- swagger/swagger.sh
+++ swagger/swagger.sh
@@ -1,7 +1,7 @@
 # Run the swagger-ui project locally, requires docker to be installed and running
 # Will pull the latest swagger-ui docker image and then will try to run it on port 9000
 #
-# Once running, it is accessible at: http://localhost/
+# Once running, it is accessible at: http://localhost:9000/
 #
 # If the user doesn't have enough permissions to use port 80, modify the local
 # port to something above 8000 that is available.
